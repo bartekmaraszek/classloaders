676042bf03666f8f4418ff4e88876dd10796e85a
Bartek Maraszek <b.maraszek@gmail.com>
2017-07-17 03:03:10 +0100

    Initial commit

---
+++ src/test/java/pl/bmaraszek/AppTest.java
@@ -0,0 +1,13 @@
+package pl.bmaraszek;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    public void shouldDoSomething() {
+        assertThat(2.0).isEqualTo(2.0);
+    }
+}
